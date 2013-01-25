f05a9b3a1dd4cf15f06a600b291dd6a0975a5710
Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
2013-01-25 23:25:40 +0100

    Add vim swp files to '.gitignore'.
    And first main files.

---
+++ heart-matters.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+#  heart-matters.py
+#
+#  Copyright 2013 Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+
+def main():
+    """ Main function, called at start :P
+    """
+    print("Welcome to Heart Matters!")
+
+# Main end
+
+# This calls the 'main' function when this script is excecuted:
+if __name__ == '__main__':
+    main()
